7e71f7945a2f0e24ba1de5d2c60aeeb5e3aa695d
haison3864 <hasan0qodirov@gmail.com>
2022-08-15 19:37:00 +0500

    first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import All from '../Main/All'
+
+const Home = () => {
+  return (
+    <div>
+        <All>
+            <h2>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus provident porro similique dicta vel ipsa facere cumque aliquid consequuntur quae doloribus eveniet voluptate, reiciendis beatae. Maiores nisi distinctio itaque accusamus!
+            </h2>
+        </All>
+        
+    </div>
+  )
+}
+
+export default Home
